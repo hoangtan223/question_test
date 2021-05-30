873f9ce92d338318debefd3d08b1c2059077c565
Tan Bui <tan.bui@employmenthero.com>
2021-05-30 22:43:07 +0700

    Chore: implement authenticate action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    api_key = request.headers['api-key']
+    @current_tenant = Tenant.find_by(api_key: api_key)
+
+    head :unauthorized unless @current_tenant
+  end
 end
