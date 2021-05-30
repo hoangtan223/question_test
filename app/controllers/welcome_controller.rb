76d491db79c575e78abea89086d513ae3f282f70
Tan Bui <tan.bui@employmenthero.com>
2021-05-31 01:29:09 +0700

    Chore: add simple UI for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_request
+
   def index
-    
+    @total_question = Question.count
+    @total_answer = Answer.count
+    @total_user = User.count
+    @total_api_call = Tenant.sum(:api_call_count)
   end
 end
