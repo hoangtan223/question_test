957e5c648af28840f80f1e454aa40a4af79afe22
Tan Bui <tan.bui@employmenthero.com>
2021-05-31 02:29:59 +0700

    Chore: refactoring question API to use query params

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe "Questions", type: :request do
   describe "GET /index" do
     let(:path) { '/questions' }
-    let!(:public_questions) { create_list(:question, 5) }
-    let!(:private_questions) { create_list(:question, 2, private: true) }
     let(:tenant) { create(:tenant) }
 
     subject { get path, headers: headers }
@@ -19,19 +17,33 @@ RSpec.describe "Questions", type: :request do
     context 'authorized request' do
       let(:headers) { { 'api-key': tenant.api_key } }
 
-      it 'returns 200' do
-        subject
-        expect(response.status).to eq(200)
+      context 'without query params' do
+        let!(:public_questions) { create_list(:question, 5) }
+        let!(:private_questions) { create_list(:question, 2, private: true) }
+
+        it 'returns 200' do
+          subject
+          expect(response.status).to eq(200)
+        end
+
+        it 'returns list of public questions' do
+          subject
+          responsed_questions = JSON.parse(response.body)
+          expect(responsed_questions.count).to eq(5)
+        end
+
+        it 'increases api_call_count by 1' do
+          expect { subject }.to change { tenant.reload.api_call_count }.by(1)
+        end
       end
 
-      it 'returns list of public questions' do
-        subject
-        responsed_questions = JSON.parse(response.body)
-        expect(responsed_questions.count).to eq(5)
-      end
+      context 'with query params' do
+        let(:path) { '/questions?q=hello' }
 
-      it 'increases api_call_count by 1' do
-        expect { subject }.to change { tenant.reload.api_call_count }.by(1)
+        it 'returns 404' do
+          subject
+          expect(response.status).to eq(404)
+        end
       end
     end
   end
